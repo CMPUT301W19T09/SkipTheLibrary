49aaf5133a05945d50a054d42c8ba5fd20049d0c
Tymoore Jamal <tymoore@ualberta.ca>
2019-02-15 17:07:21 -0700

    added book template test

---
+++ app/src/test/java/com/stl/skipthelibrary/BookTest.java
@@ -0,0 +1,32 @@
+package com.stl.skipthelibrary;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BookTest {
+
+    @Test
+    public void getImages() {
+    }
+
+    @Test
+    public void setImages() {
+    }
+
+    @Test
+    public void getDescription() {
+    }
+
+    @Test
+    public void setDescription() {
+    }
+
+    @Test
+    public void getUserID() {
+    }
+
+    @Test
+    public void setUserID() {
+    }
+}
