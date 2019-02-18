916e7cbb6438e4a8a7bb7c1fba5ab814febcaef3
LukeSlev <lslevins@ualberta.ca>
2019-02-18 14:50:03 -0700

    add first iteration of database helper and viewable image

--- app/src/main/java/com/stl/skipthelibrary/ViewableImage.java
+++ app/src/main/java/com/stl/skipthelibrary/ViewableImage.java
@@ -1,4 +1,37 @@
 package com.stl.skipthelibrary;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.util.Base64;
+
+import java.io.ByteArrayOutputStream;
+
 public class ViewableImage {
+    private String imageString;
+
+    public ViewableImage(Uri uri) {
+        Bitmap bm = BitmapFactory.decodeFile(uri.toString());
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bm.compress(Bitmap.CompressFormat.JPEG, 100, baos); //bm is the bitmap object
+        byte[] b = baos.toByteArray();
+    }
+
+    public String getImageString() {
+        return imageString;
+    }
+
+    public void setImageString(String imageString) {
+        this.imageString = imageString;
+    }
+
+    public void encode(byte[] byteArrayImage){
+        setImageString(Base64.encodeToString(byteArrayImage, Base64.DEFAULT));
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public void decode(){
+//        Base64.encode(getImageString(),Base64.DEFAULT);
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
 }
