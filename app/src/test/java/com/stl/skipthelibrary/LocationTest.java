40c9df03953d1ab1335c76e3438fcb9651f5c22d
Tymoore Jamal <tymoore@ualberta.ca>
2019-02-18 16:26:30 -0700

    added location test case

--- app/src/test/java/com/stl/skipthelibrary/LocationTest.java
+++ app/src/test/java/com/stl/skipthelibrary/LocationTest.java
@@ -1,24 +1,25 @@
 package com.stl.skipthelibrary;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class LocationTest {
+    private Location location;
+    private double testLat;
+    private double testLon;
 
-    @Test
-    public void getLatitude() {
-    }
-
-    @Test
-    public void setLatitude() {
-    }
-
-    @Test
-    public void getLongitude() {
+    @Before
+    public void setup(){
+        testLat = 53.525151;
+        testLon = -113.527350;
+        location = new Location(testLat, testLon);
     }
 
     @Test
     public void setLongitude() {
+        assertEquals(testLat, location.getLatitude(), 0);
+        assertEquals(testLon, location.getLongitude(), 0);
     }
 }
