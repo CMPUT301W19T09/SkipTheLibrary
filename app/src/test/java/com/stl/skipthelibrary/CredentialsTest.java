148e2abe7d6bbe8394bc1db16e72cdf7020cce46
Tymoore Jamal <tymoore@ualberta.ca>
2019-02-18 16:19:14 -0700

    added credentials test cases

--- app/src/test/java/com/stl/skipthelibrary/CredentialsTest.java
+++ app/src/test/java/com/stl/skipthelibrary/CredentialsTest.java
@@ -3,35 +3,25 @@ package com.stl.skipthelibrary;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.UUID;
+
 import static org.junit.Assert.*;
 
 public class CredentialsTest {
+    private Credentials credentials;
+    private UUID uuid;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp(){
+        uuid = UUID.randomUUID();
+        credentials = new Credentials("test userName", uuid, "test hashedPassword");
     }
 
     @Test
-    public void getUserName() {
-    }
+    public void testConstructor() {
+        assertEquals("test userName", credentials.getUserName());
+        assertEquals(uuid, credentials.getSecretID());
+        assertEquals("test hashedPassword", credentials.getHashedPassword());
 
-    @Test
-    public void setUserName() {
-    }
-
-    @Test
-    public void getSecretID() {
-    }
-
-    @Test
-    public void setSecretID() {
-    }
-
-    @Test
-    public void getHashedPassword() {
-    }
-
-    @Test
-    public void setHashedPassword() {
     }
 }
