637b2b33c34f22628e8fc6085dabecf0817b8c38
Tymoore Jamal <tymoore@ualberta.ca>
2019-02-19 00:58:41 -0700

    added logging in with Firebase Auth

--- app/src/main/java/com/stl/skipthelibrary/LoginActivity.java
+++ app/src/main/java/com/stl/skipthelibrary/LoginActivity.java
@@ -1,37 +1,59 @@
 package com.stl.skipthelibrary;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import com.google.android.material.button.MaterialButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
 
-        MaterialButton signIn = findViewById(R.id.SignInButton);
-        signIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, NotificationActivity.class);
-                startActivity(intent);
-            }
-        });
 
-        MaterialButton signUp = findViewById(R.id.SignUpButton);
-        signUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
-                startActivity(intent);
-            }
-        });
+
+
+
+    public void signInOnClick(View view) {
+        firebaseAuth = FirebaseAuth.getInstance();
+        String email = ((EditText) findViewById(R.id.UsernameEditText)).getText().toString();
+        String password = ((EditText) findViewById(R.id.PasswordEditText)).getText().toString();
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            Toast.makeText(LoginActivity.this, "Authentication successful.", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(LoginActivity.this, NotificationActivity.class);
+                            // TODO: Get the user from their userID and pass the user object to the intent, or write it somewhere as a global var
+                            startActivity(intent);
+                        }
+                        else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    public void signUpOnClick(View view) {
+        Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+        startActivity(intent);
     }
 }
