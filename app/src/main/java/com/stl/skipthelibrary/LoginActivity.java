6536fdf61a8d82085078e0ac7d11eb362a96bcfa
Tymoore Jamal <tymoore@ualberta.ca>
2019-02-18 21:02:49 -0700

    now starts at login screen

---
+++ app/src/main/java/com/stl/skipthelibrary/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.stl.skipthelibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.button.MaterialButton;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        MaterialButton signIn = findViewById(R.id.SignInButton);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, NotificationActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
