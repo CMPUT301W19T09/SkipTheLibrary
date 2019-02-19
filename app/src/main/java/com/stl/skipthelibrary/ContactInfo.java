4410d7619dd994937432287dcc217374bc859383
Gillian Pierce <gmpierce@ualberta.ca>
2019-02-18 17:53:06 -0700

    fixed some funky stuff with xml and androidx

--- app/src/main/java/com/stl/skipthelibrary/ContactInfo.java
+++ app/src/main/java/com/stl/skipthelibrary/ContactInfo.java
@@ -1,11 +1,8 @@
 package com.stl.skipthelibrary;
 
-import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.support.v4.app.ActivityCompat;
 
 public class ContactInfo {
     private String email;
