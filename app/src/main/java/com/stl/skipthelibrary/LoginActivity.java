7d0b81736c6f5205f3409b3690321a27e7dee61e
Tymoore Jamal <tymoore@ualberta.ca>
2019-02-19 12:34:46 -0700

    made suggested changes

--- app/src/main/java/com/stl/skipthelibrary/LoginActivity.java
+++ app/src/main/java/com/stl/skipthelibrary/LoginActivity.java
@@ -11,19 +11,20 @@ import android.widget.Toast;
 public class LoginActivity extends AppCompatActivity {
 private EditText passwordText;
 private EditText usernameText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        EditText usernameText = findViewById(R.id.UsernameEditText);
-        EditText password = findViewById(R.id.PasswordEditText);
+        usernameText = findViewById(R.id.UsernameEditText);
+        passwordText = findViewById(R.id.PasswordEditText);
         
     }
 
 
     public void signInOnClick(View view) {
         String email = usernameText.getText().toString();
-        String password = ((EditText) findViewById(R.id.PasswordEditText)).getText().toString();
+        String password = passwordText.getText().toString();
         SignInValidator signInValidator = new SignInValidator(email, password);
         if (signInValidator.isValid()){
             DatabaseHelper databaseHelper = new DatabaseHelper(this);
--- app/src/main/java/com/stl/skipthelibrary/SignUpActivity.java
+++ app/src/main/java/com/stl/skipthelibrary/SignUpActivity.java
@@ -1,41 +1,41 @@
 package com.stl.skipthelibrary;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.UUID;
 
 public class SignUpActivity extends AppCompatActivity {
-    public String TAG = getClass().getSimpleName();
+    EditText usernameText;
+    EditText passwordText;
+    EditText firstNameText;
+    EditText lastNameText;
+    EditText emailAddressText;
+    EditText phoneNumberText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        usernameText = findViewById(R.id.SignUpUsername);
+        passwordText = findViewById(R.id.SignUpPassword);
+        firstNameText = findViewById(R.id.SignUpFirstName);
+        lastNameText = findViewById(R.id.SignUpLastName);
+        emailAddressText = findViewById(R.id.SignUpEmail);
+        phoneNumberText = findViewById(R.id.SignUpPhoneNumber);
     }
 
     public void RegisterOnClick(View view) {
-        String userName = ((EditText) findViewById(R.id.SignUpUsername)).getText().toString();
-        String password = ((EditText) findViewById(R.id.SignUpPassword)).getText().toString();
-        String firstName = ((EditText) findViewById(R.id.SignUpFirstName)).getText().toString();
-        String lastName = ((EditText) findViewById(R.id.SignUpLastName)).getText().toString();
-        String emailAddress = ((EditText) findViewById(R.id.SignUpEmail)).getText().toString();
-        String phoneNumber = ((EditText) findViewById(R.id.SignUpPhoneNumber)).getText().toString();
+        String userName = usernameText.getText().toString();
+        String password = passwordText.getText().toString();
+        String firstName = firstNameText.getText().toString();
+        String lastName = lastNameText.getText().toString();
+        String emailAddress = emailAddressText.getText().toString();
+        String phoneNumber = phoneNumberText.getText().toString();
         SignUpValidator signUpValidator = new SignUpValidator(userName, password, firstName, lastName, emailAddress, phoneNumber);
         if (signUpValidator.isValid()){
             DatabaseHelper databaseHelper = new DatabaseHelper(this);
