44b941d44b8a651904b89fc218886b9c24903aa8
Tymoore Jamal <tymoore@ualberta.ca>
2019-02-19 22:53:03 -0700

    added recycler view to owner

---
+++ app/src/main/java/com/stl/skipthelibrary/BookRecyclerAdapter.java
@@ -0,0 +1,85 @@
+package com.stl.skipthelibrary;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class BookRecyclerAdapter extends RecyclerView.Adapter<BookRecyclerAdapter.ViewHolder>{
+    private Context context;
+    private ArrayList<Book> books;
+
+    public BookRecyclerAdapter(Context context, ArrayList<Book> books) {
+        this.context = context;
+        this.books = books;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.book_list_item, parent,false);
+        return new BookRecyclerAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        String title = books.get(position).getDescription().getTitle();
+        String author = books.get(position).getDescription().getAuthor();
+        String status = books.get(position).getRequests().getState().getBookStatus().name();
+
+        holder.title.setText(title);
+        holder.author.setText(author);
+        holder.status.setText(status);
+    }
+
+    @Override
+    public int getItemCount() {
+        return books.size();
+    }
+
+
+    // Inner Class ViewHolder defines all of the elements in the corresponding xml file.
+    // it allows us to set their properties during onBind.
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        LinearLayout parentLayout;
+        ImageView rightArrow;
+        TextView title;
+        TextView author;
+        TextView status;
+
+        // ViewHolder constructor
+        public ViewHolder(View itemView) {
+            super(itemView);
+            parentLayout = itemView.findViewById(R.id.book_list_parent_view);
+            rightArrow = itemView.findViewById(R.id.BookListItemRightArrow);
+            title = itemView.findViewById(R.id.BookListItemTitle);
+            author = itemView.findViewById(R.id.BookListItemAuthor);
+            status = itemView.findViewById(R.id.BookListItemStatus);
+        }
+
+    }
+
+    public Context getContext() {
+        return context;
+    }
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
+
+    public ArrayList<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(ArrayList<Book> books) {
+        this.books = books;
+    }
+}
--- app/src/main/java/com/stl/skipthelibrary/MyBooksActivity.java
+++ app/src/main/java/com/stl/skipthelibrary/MyBooksActivity.java
@@ -4,7 +4,11 @@ import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.ArrayList;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MyBooksActivity extends AppCompatActivity {
 
@@ -16,5 +20,44 @@ public class MyBooksActivity extends AppCompatActivity {
         BottomNavigationView navigation = findViewById(R.id.bottom_navigation);
         navigation.setOnNavigationItemSelectedListener(new NavigationHandler(this));
         navigation.setSelectedItemId(R.id.my_books);
+
+        initRecyclerView();
     }
+
+    private void initRecyclerView(){
+        ArrayList<Book> books = new ArrayList<Book>();
+        books.add(new Book(new BookDescription("test Title", "test Sysnopsis",
+                "test author", new Rating()),"testUsername",
+                new RequestHandler(new State(BookStatus.ACCEPTED,null, null)), null, new Rating()));
+        books.add(new Book(new BookDescription("test Title", "test Sysnopsis",
+                "test author", new Rating()),"testUsername",
+                new RequestHandler(new State(BookStatus.REQUESTED,null, null)), null, new Rating()));
+        books.add(new Book(new BookDescription("test Title", "test Sysnopsis",
+                "test author", new Rating()),"testUsername",
+                new RequestHandler(new State(BookStatus.AVAILABLE,null, null)), null, new Rating()));
+        books.add(new Book(new BookDescription("test Title", "test Sysnopsis",
+                "test author", new Rating()),"testUsername",
+                new RequestHandler(new State(BookStatus.BORROWED,null, null)), null, new Rating()));
+        books.add(new Book(new BookDescription("test Title", "test Sysnopsis",
+                "test author", new Rating()),"testUsername",
+                new RequestHandler(new State(BookStatus.REQUESTED,null, null)), null, new Rating()));
+        books.add(new Book(new BookDescription("test Title", "test Sysnopsis",
+                "test author", new Rating()),"testUsername",
+                new RequestHandler(new State(BookStatus.REQUESTED,null, null)), null, new Rating()));
+        books.add(new Book(new BookDescription("test Title", "test Sysnopsis",
+                "test author", new Rating()),"testUsername",
+                new RequestHandler(new State(BookStatus.REQUESTED,null, null)), null, new Rating()));
+        books.add(new Book(new BookDescription("test Title", "test Sysnopsis",
+                "test author", new Rating()),"testUsername",
+                new RequestHandler(new State(BookStatus.REQUESTED,null, null)), null, new Rating()));
+        books.add(new Book(new BookDescription("test Title", "test Sysnopsis",
+                "test author", new Rating()),"testUsername",
+                new RequestHandler(new State(BookStatus.REQUESTED,null, null)), null, new Rating()));
+
+        RecyclerView recyclerView = findViewById(R.id.ownerBooksRecyclerView);
+        BookRecyclerAdapter adapter = new BookRecyclerAdapter(this, books);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
 }
