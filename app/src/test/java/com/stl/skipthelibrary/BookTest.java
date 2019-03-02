bfedd2eec3c726731f614fb0f7e4698f12d21e9a
Tymoore Jamal <tymoore@ualberta.ca>
2019-03-01 18:03:40 -0700

    Travis be nice please

--- app/src/test/java/com/stl/skipthelibrary/BookTest.java
+++ app/src/test/java/com/stl/skipthelibrary/BookTest.java
@@ -29,7 +29,7 @@ public class BookTest {
 
     @Test
     public void testSetUUID() {
-        UUID uuid = UUID.randomUUID();
+        String uuid = UUID.randomUUID().toString();
         book.setUuid(uuid);
         assertEquals(uuid, book.getUuid());
     }
