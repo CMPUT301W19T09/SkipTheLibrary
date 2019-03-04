94e913984fd3fee8aad135bfa3a6d877ffc45b20
QFD-Felix <qfdfelix@gmail.com>
2019-03-03 17:08:41 -0700

    Button UI implemented

--- app/src/main/java/com/stl/skipthelibrary/ViewBookActivity.java
+++ app/src/main/java/com/stl/skipthelibrary/ViewBookActivity.java
@@ -3,12 +3,19 @@ package com.stl.skipthelibrary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ViewBookActivity extends AppCompatActivity {
-
+    private Button button;
+    private View view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.book_details);
+
+
+        button = (Button) findViewById(R.id.HandOffButton);
+        button.setText("lend");
     }
 }
