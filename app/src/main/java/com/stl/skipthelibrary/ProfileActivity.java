73b5f3156920f757e145ed434410343283dc5201
Tymoore Jamal <tymoore@ualberta.ca>
2019-03-05 13:40:49 -0700

    started but hit image roadblock

--- app/src/main/java/com/stl/skipthelibrary/ProfileActivity.java
+++ app/src/main/java/com/stl/skipthelibrary/ProfileActivity.java
@@ -1,5 +1,6 @@
 package com.stl.skipthelibrary;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -10,16 +11,22 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
 
-public class MyProfileActivity extends AppCompatActivity {
-    DatabaseHelper databaseHelper;
+public class ProfileActivity extends AppCompatActivity {
+    public final static String USER_NAME = "userName";
+    private static final String TAG = ProfileActivity.class.getSimpleName();
+
+    private DatabaseHelper databaseHelper;
     private CircleImageView myProfileImage;
     private TextView myProfileName;
     private TextView myProfileUsername;
     private TextView myProfileEmail;
     private TextView myProfilePhoneNumber;
     private BottomNavigationView navigation;
-    private User currentUser;
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,16 +44,34 @@ public class MyProfileActivity extends AppCompatActivity {
         navigation.setOnNavigationItemSelectedListener(new NavigationHandler(this));
         navigation.setSelectedItemId(R.id.profile);
 
-        currentUser = CurrentUser.getInstance();
-        currentUser.getContactInfo().setContext(this);
+//        Intent intent = getIntent();
+//        setUser(intent.getExtras().getString(USER_NAME));
 
-        myProfileImage.setImageBitmap(currentUser.getImage().decode());
+        user.getContactInfo().setContext(this);
 
-        myProfileName.setText(currentUser.getName());
-        myProfileUsername.setText("@"+currentUser.getUserName());
-        myProfileEmail.setText("Email: "+currentUser.getContactInfo().getEmail());
-        myProfilePhoneNumber.setText("Phone Number: "+currentUser.getContactInfo().getPhoneNumber().replaceFirst("(\\d{3})(\\d{3})(\\d+)", "($1) $2-$3"));
+        myProfileImage.setImageBitmap(user.getImage().decode());
 
+        myProfileName.setText(user.getName());
+        myProfileUsername.setText("@"+ user.getUserName());
+        myProfileEmail.setText("Email: "+ user.getContactInfo().getEmail());
+        myProfilePhoneNumber.setText("Phone Number: "+ user.getContactInfo().getPhoneNumber().replaceFirst("(\\d{3})(\\d{3})(\\d+)", "($1) $2-$3"));
+
+    }
+
+    private void setUser(String userName) {
+
+    }
+
+    private void userRetrieved(User retrievedUser) {
+        user = retrievedUser;
+        user.getContactInfo().setContext(this);
+
+        myProfileImage.setImageBitmap(user.getImage().decode());
+
+        myProfileName.setText(user.getName());
+        myProfileUsername.setText("@"+ user.getUserName());
+        myProfileEmail.setText("Email: "+ user.getContactInfo().getEmail());
+        myProfilePhoneNumber.setText("Phone Number: "+ user.getContactInfo().getPhoneNumber().replaceFirst("(\\d{3})(\\d{3})(\\d+)", "($1) $2-$3"));
     }
 
 
@@ -58,11 +83,11 @@ public class MyProfileActivity extends AppCompatActivity {
     }
 
     public void sendEmailOnClick(View view) {
-        currentUser.getContactInfo().startEmail();
+        user.getContactInfo().startEmail();
     }
 
     public void sendPhoneCallOnClick(View view) {
-        currentUser.getContactInfo().startCall();
+        user.getContactInfo().startCall();
     }
 
     @Override
