19b27492b126dd45e9e568d50879a24daa0a0d36
QFD-Felix <qfdfelix@gmail.com>
2019-03-05 23:54:35 -0700

    Into the scaning screen

--- app/src/main/java/com/stl/skipthelibrary/ViewBookActivity.java
+++ app/src/main/java/com/stl/skipthelibrary/ViewBookActivity.java
@@ -2,7 +2,9 @@ package com.stl.skipthelibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -18,4 +20,28 @@ public class ViewBookActivity extends AppCompatActivity {
         button = (Button) findViewById(R.id.HandOffButton);
         button.setText("lend");
     }
+//    public void scanBookOnClick(View view) {
+//        Intent intent = new Intent(this, ScannerActivity.class);
+//        startActivityForResult(intent, ScannerActivity.SCAN_BOOK);
+//    }
+//
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if(requestCode == ScannerActivity.SCAN_BOOK) {
+//            if (resultCode == RESULT_OK) {
+//                String ISBN = data.getStringExtra("ISBN");
+//                bookISBN.setText(ISBN);
+//                new BookDescriptionReceiver(ISBN, bookTitle, bookAuthor, bookDesc).execute(ISBN);
+//            } else {
+//                Log.d(TAG, "onActivityResult: Something went wrong in scan");
+//            }
+//        }
+//    }
+
+    public void handOffButtonOnClick(View view){
+        Intent intent = new Intent(this, ScannerActivity.class);
+        startActivityForResult(intent, ScannerActivity.SCAN_BOOK);
+    }
+
 }
