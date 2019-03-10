1ea7819455b74dfee591d55c4bfb5e6cb5990125
LukeSlev <lslevins@ualberta.ca>
2019-03-09 18:40:58 -0700

    the camera kit seems to be very very flakey so why don't we just use the normal camera

--- app/src/main/java/com/stl/skipthelibrary/Activities/ScannerActivity.java
+++ app/src/main/java/com/stl/skipthelibrary/Activities/ScannerActivity.java
@@ -4,16 +4,20 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Camera;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
-import com.camerakit.CameraKitView;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.ml.vision.FirebaseVision;
@@ -23,15 +27,20 @@ import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOption
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.stl.skipthelibrary.R;
 
+
+
 import java.util.List;
 
 /**
  * The scanner activity launches the scanner and returns the ISBN of the scanned barcode
  */
 public class ScannerActivity extends AppCompatActivity {
-    public static final int SCAN_BOOK = 3;
+    public static final String TAG = ScannerActivity.class.getSimpleName();
 
-    private CameraKitView cameraKitView;
+    public static final int SCAN_BOOK = 3;
+    public static final int SCAN = 4;
+
+    private Uri imageUri;
 
     /**
      * Bind UI elements and setup listners and begin scanning
@@ -42,8 +51,6 @@ public class ScannerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanner);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        cameraKitView = findViewById(R.id.scanner_camera_view);
-        cameraKitView.onStart();
     }
 
     /**
@@ -51,12 +58,14 @@ public class ScannerActivity extends AppCompatActivity {
      * @param view: the scan now button
      */
     public void confirmScanOnClick(View view) {
-        cameraKitView.captureImage(new CameraKitView.ImageCallback() {
-            @Override
-            public void onImage(CameraKitView cameraKitView, byte[] bytes) {
-                processBitMap(BitmapFactory.decodeByteArray(bytes, 0, bytes.length));
-            }
-        });
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "New Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From your Camera");
+        imageUri = getContentResolver().insert(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent, ScannerActivity.SCAN);
     }
 
     /**
@@ -65,6 +74,7 @@ public class ScannerActivity extends AppCompatActivity {
      * @param bitmap: the bitmap to process
      */
     private void processBitMap(Bitmap bitmap){
+        Log.d(TAG, "processBitMap: PROCESS");
         FirebaseVisionImage firebaseVisionImage = FirebaseVisionImage.fromBitmap(bitmap);
         FirebaseVisionBarcodeDetectorOptions options = new FirebaseVisionBarcodeDetectorOptions.Builder()
                 .setBarcodeFormats(FirebaseVisionBarcode.FORMAT_ALL_FORMATS).build();
@@ -74,6 +84,7 @@ public class ScannerActivity extends AppCompatActivity {
                 .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionBarcode>>() {
                     @Override
                     public void onSuccess(List<FirebaseVisionBarcode> firebaseVisionBarcodes) {
+                        Log.d(TAG, "onSuccess: NICE");
                         processResult(firebaseVisionBarcodes);
                     }
                 }).addOnFailureListener(new OnFailureListener() {
@@ -99,7 +110,6 @@ public class ScannerActivity extends AppCompatActivity {
                 Intent intent = new Intent();
                 intent.putExtra("ISBN", barcode.getDisplayValue());
                 setResult(Activity.RESULT_OK, intent);
-                cameraKitView.onStop();
                 finish();
                 return;
             }
@@ -107,10 +117,35 @@ public class ScannerActivity extends AppCompatActivity {
         Toast.makeText(this, "Found improper barcode. Please try again", Toast.LENGTH_SHORT).show();
     }
 
-
+    /**
+     * Retrieve request's made to other activities.
+     * @param requestCode: the request code made
+     * @param resultCode: the returned code
+     * @param data: the intent passed back
+     */
     @Override
-    public void finish() {
-        cameraKitView.onStop();
-        super.finish();
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // if the request is from the scanner
+        if (requestCode == ScannerActivity.SCAN) {
+            // if successful
+            if (resultCode == RESULT_OK) {
+                // get the scanned ISBN
+                try {
+                    Bitmap thumbnail = MediaStore.Images.Media.getBitmap(
+                            getContentResolver(), imageUri);
+                    Toast.makeText(getApplicationContext(),"RECEIVED SOMETHING", Toast.LENGTH_SHORT).show();
+                    processBitMap(thumbnail);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            } else {
+                Log.d(TAG, "onActivityResult: Something went wrong in scan");
+            }
+        } else {
+            Log.d(TAG, "onActivityResult: Error in picking image");
+        }
     }
+
 }
